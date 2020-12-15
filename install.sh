5a551fc39bb529b8216bf89520e1401b993ee2d5
JaehyunShim <jhshim@robotis.com>
2020-12-15 20:21:32 +0900

    minor change

--- install.sh
+++ install.sh
@@ -10,17 +10,16 @@ bash ./install/install_terminator.sh
 bash ./install/install_vscode.sh
 bash ./install/install_docker.sh
 
-ln config/autostart/barrier.desktop ~/.config/autostart/barrier.desktop
-ln config/autostart/vino-server.desktop ~/.config/autostart/vino-server.desktop
-ln config/Code/User/settings.json ~/.config/Code/User/settings.json
-ln config/terminator/config ~/.config/terminator/config
+ln -p config/autostart/barrier.desktop ~/.config/autostart/barrier.desktop
+ln -p config/autostart/vino-server.desktop ~/.config/autostart/vino-server.desktop
+ln -p config/Code/User/settings.json ~/.config/Code/User/settings.json
+ln -p config/terminator/config ~/.config/terminator/config
+ln -p config/git/gitconfig ~/.gitconfig
 
-ln config/git/gitconfig ~/.gitconfig
-
-if [ 'lsb_release -cs' == xenial ]; then
+if [ `lsb_release -cs` == xenial ]; then
   cat config/bash/bashrc16.04 >> ~/.bashrc
-elif [ 'lsb_release -cs' == bionic ]; then
+elif [ `lsb_release -cs` == bionic ]; then
   cat config/bash/bashrc18.04 >> ~/.bashrc
-elif [ 'lsb_release -cs' == focal ]; then
+elif [ `lsb_release -cs` == focal ]; then
   cat config/bash/bashrc20.04 >> ~/.bashrc
 fi
