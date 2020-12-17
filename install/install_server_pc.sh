09e6478c2c75f4b87e759c6d969d914cca7757dc
JaehyunShim <jhshim@robotis.com>
2020-12-17 21:28:49 +0900

    add samba to install_server_pc.sh

--- install/install_server_pc.sh
+++ install/install_server_pc.sh
@@ -5,6 +5,12 @@
 # SSH
 sudo apt-get install openssh-server
 
+# Samba
+$ sudo apt-get install -y samba
+$ echo 'password' | sudo -S smbpasswd -n username
+$ sudo bash -c 'cat config/samba/smb.conf >> /etc/samba/smb.conf'
+$ echo 'password' | sudo -S service smbd restart
+
 # Vino
 sudo apt install vino
 gsettings set org.gnome.Vino require-encryption false
