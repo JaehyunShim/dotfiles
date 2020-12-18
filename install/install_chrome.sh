46bc5eee92d7b04fab811eafb12a827ac7113292
JaehyunShim <jhshim@robotis.com>
2020-12-18 11:35:41 +0900

    fix bug in install_chrome

--- install/install_chrome.sh
+++ install/install_chrome.sh
@@ -2,7 +2,6 @@
 # MIT License
 # Copyright (c) 2020 Jaehyun Shim
 
-wget -q -O-https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add-
-sudo sh -c 'echo "deb [arch = amd64] http://dl.google.com/linux/chrome/deb/ stable main">> /etc/apt/sources.list.d/google-chrome.list '
-sudo apt-get update -y
-sudo apt-get install -y google-chrome-stable
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get -y install ./google-chrome-stable_current_amd64.deb
+rm ./google-chrome-stable_current_amd64.deb
