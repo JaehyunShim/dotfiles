590374f8c0f18b2e435185fa46c04e756d5f009e
Jaehyun Shim <38637417+JaehyunShim@users.noreply.github.com>
2021-06-08 13:23:06 +0900

    Update install_ros2.sh

--- install/install_ros2.sh
+++ install/install_ros2.sh
@@ -2,25 +2,29 @@
 # MIT License
 # Copyright (c) 2021 Jaehyun Shim
 
-sudo apt-get -y update && sudo apt-get -y install locales
+# Reference: https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html
+
+# Set Locale
+sudo apt update && sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
+
+# Setup Sources
 sudo apt update && sudo apt install -y curl gnupg2 lsb-release
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-sudo apt-get -y update
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-if [ `lsb_release -cs` == focal ]; then
-  sudo apt install -y ros-foxy-desktop
-  source /opt/ros/foxy/setup.bash
-fi
+# Install ROS 2 packages
+sudo apt update -y
+sudo apt install -y ros-foxy-desktop
 
-sudo apt install -y python3-pip
-pip3 install -U argcomplete
-
+# Environment setup
+source /opt/ros/foxy/setup.bash
+sudo apt install -y python3-argcomplete
 sudo apt install -y python3-colcon-common-extensions
 
+# Create workspace
 mkdir -p $HOME/colcon_ws/src
 cd $HOME/colcon_ws
 colcon build --symlink-install
