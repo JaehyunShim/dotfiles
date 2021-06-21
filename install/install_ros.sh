80a22b54fc40c3bc9e60c7589ddffd345b0639bc
Jaehyun Shim <38637417+JaehyunShim@users.noreply.github.com>
2021-06-21 16:47:08 +0100

    Update install_ros.sh

--- install/install_ros.sh
+++ install/install_ros.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2021 Jaehyun Shim
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get -y update
 
 if [ `lsb_release -cs` == focal ]; then
@@ -13,7 +13,7 @@ if [ `lsb_release -cs` == focal ]; then
   #sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 fi
 
-echo 'password' | sudo -S rosdep init
+sudo -S rosdep init
 rosdep update
 
 mkdir -p $HOME/catkin_ws/src
